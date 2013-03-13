a9813e63e23790ce8634fa1fca5544d168e9ba2e
GT <gthandavam@gmail.com>
2013-03-13 11:24:36 -0400

    added a simple API to fetch words to be disambiguated from the test-data - The api returns a dictionary-(a hash data structure) with keys as id of head tags in the document and value as the words to be disambiguated

--- gt/wsd/hw1/graphwsd/parsesemeval/parse_semeval.py
+++ gt/wsd/hw1/graphwsd/parsesemeval/parse_semeval.py
@@ -5,14 +5,34 @@ Created on Mar 12, 2013
 '''
 import xml.etree.ElementTree as etree
 
-if __name__ == '__main__':
-    tree = etree.parse('/Users/ganesathandavamponnuraj/Downloads/task17-test+keys/test/English/EnglishAW.test.xml')
-    
+'''
+    @param - filename @type string -> filename path that contains the sem-eval test data [EnglishAW.text.xml file]
+    @return - returns a dictionary: key -> id of the head tag in the sem-eval task 17 document @type string
+                                    value-> word to be disambiguated @type string
+'''
+def get_wsd_data(filename):
+    tree = etree.parse(filename)
+        
+# xml file comes without a namespace; so looking up for all the head tags within the document,
+#without specifying the namespace
+#Ref: http://getpython3.com/diveintopython3/xml.html#xml-parse
+
     heads = tree.findall("//head")    
     
+    ret_dictionary = {}
+    
     for head in heads:
-        print head.attrib," " + head.text
+#        print head.attrib['id']," " + head.text
+        ret_dictionary[head.attrib['id']] = head.text
         
+    return ret_dictionary    
+        
+ 
+        
+if __name__ == '__main__':
+    dictionary = get_wsd_data('/Users/ganesathandavamponnuraj/Downloads/task17-test+keys/test/English/EnglishAW.test.xml');
+    for key in dictionary.keys():
+        print key," " + dictionary[key]
         
     
     
