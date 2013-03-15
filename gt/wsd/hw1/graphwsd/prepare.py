8a9d412886ede26b0860aabd7386b8b80de53358
Sivaranjini <drouppa@gmail.com>
2013-03-15 04:25:37 -0700

    Pageranked senses for a single sentence - WIP - need to include window range for edges, and print the answers in a key

--- gt/wsd/hw1/graphwsd/prepare.py
+++ gt/wsd/hw1/graphwsd/prepare.py
@@ -8,6 +8,8 @@ from nltk.corpus import wordnet as wn
 from collections import defaultdict
 from nltk.stem import WordNetLemmatizer
 import xml.etree.ElementTree as etree
+import numbers
+import gt.wsd.hw1.graphwsd.pagerank.pagerank as pr
 '''
 idea: 
 
@@ -49,35 +51,8 @@ def process_per_sentence(filename):
 #        
 #        print text
 #    
-    graph_matrix = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-                    ]
     
-    words = ['are', 'lessons', 'nature', 'conservation']
+    words = ['church', 'bell', 'rung', 'sunday']
     word_synsets = {}
     synset_index = {}
     index = 0
@@ -85,8 +60,14 @@ def process_per_sentence(filename):
         word_synsets[word] = wn.synsets(word)
         for synset in word_synsets[word]:
             synset_index[synset] = index
+            print synset, index
             index += 1
 #        length += len(word_synsets[word])
+    graph_matrix = [[0 for i in range(index)] for j in range(index)]
+    for i in range(index):
+        print [graph_matrix[i][j] for j in range(index)]   
+
+    print index
 
     for word1 in words:
         for word2 in words:
@@ -94,19 +75,29 @@ def process_per_sentence(filename):
                 for synset1 in word_synsets[word1]:
                     for synset2 in word_synsets[word2]:
                         sim = synset1.wup_similarity(synset2)
-                        if sim == 'none':
+                        if isinstance(sim, numbers.Number) == False:
                             sim = 0
                         graph_matrix[synset_index[synset1]][synset_index[synset2]] = sim
                         graph_matrix[synset_index[synset2]][synset_index[synset1]] = sim
     
     for i in range(26):
         print [graph_matrix[i][j] for j in range(26)]   
- 
+    
+    ranked_sense = pr.get_pagerank(graph_matrix)
+    for word in words:
+        synsets = word_synsets[word]
+        max_r = 0
+        max_index = -1
+        for synset in synsets:
+            if ranked_sense[synset_index[synset]] >= max_r:
+                max_r = ranked_sense[synset_index[synset]]
+                max_index = synset_index[synset]
+        print max_index, max_r    
          
 if __name__ == '__main__':
-    print "page rank module"
+#    print "page rank module"
     
-    print wn.synsets("dog", "n") #why eclipse marking it as error ?
+#    print wn.synsets("dog", "n") #why eclipse marking it as error ?
     
 #    print [sense.offset for sense in wn.synsets("dog")]
     process_per_sentence("/home/aravindous/GT-CompLing/test/English/EnglishAW.test-sample.xml")
